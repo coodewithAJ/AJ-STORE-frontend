7dce3da33cbe3d21db2f5d376ee22089656298a8
coodewithAJ <75101047+coodewithAJ@users.noreply.github.com>
2022-10-02 16:55:27 +0530

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = "https://aj-store-backend.onrender.com/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMWFhOWU5NTVmNmUxMWUwOGUyOWExMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MjgxMDk1MX0.hkZqjTHnOVmcG0STZr6aHuENTxdQ8tAYlJgbMU7iTC4"
 
 export const publicRequest = axios.create({
