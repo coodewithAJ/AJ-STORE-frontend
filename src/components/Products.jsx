0eacb5c68268a732f235b2eebe2458b79b9d20d6
coodewithAJ <75101047+coodewithAJ@users.noreply.github.com>
2022-10-02 17:54:04 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,7 +20,7 @@ const Products = ({cat,sort,filters}) => {
   useEffect(()=>{
     const getProducts = async()=>{
       try{
-        const res = await axios.get( cat ? `http://localhost:5000/api/products?category=${cat}` : "http://localhost:5000/api/products")
+        const res = await axios.get( cat ? `https://aj-store-backend.onrender.com/api/products?category=${cat}` : "https://aj-store-backend.onrender.com/api/products")
 
         setProducts(res.data)
 
